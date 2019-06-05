ead64994a3071ae2ef75b11d16cf0254c90ab4bf
cschleiden <cschleiden@live.de>
2019-06-04 18:53:14 -0700

    Updates

---
+++ precache-manifest.17a05947498badeb4a12cfb277c458e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7880fbba40839f7e6ce9fd2b37c39ea4",
+    "url": "/prototype/index.html"
+  },
+  {
+    "revision": "9f73479a45db3bafd6fe",
+    "url": "/prototype/static/css/2.840a1a99.chunk.css"
+  },
+  {
+    "revision": "e13673a64382bac73296",
+    "url": "/prototype/static/css/main.3e75b063.chunk.css"
+  },
+  {
+    "revision": "9f73479a45db3bafd6fe",
+    "url": "/prototype/static/js/2.593f63d5.chunk.js"
+  },
+  {
+    "revision": "e13673a64382bac73296",
+    "url": "/prototype/static/js/main.d3c78d5c.chunk.js"
+  },
+  {
+    "revision": "52ef3d339a42f4a1675e",
+    "url": "/prototype/static/js/runtime-main.d68e0bf9.js"
+  }
+]);
